b5d4776f165cca02038a2e348eada5db37c15f98
ayayasmine <yasminayabelloum1s5@gmail.com>
2024-11-28 14:27:45 +0100

    added the CI for be

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-from iebank_api import app
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+app = Flask(__name__)
+
+# Load environment variables
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+db = SQLAlchemy(app)
+
+# Import your routes and models
+from iebank_api import routes, models
 
 if __name__ == '__main__':
     app.run(debug=True)
